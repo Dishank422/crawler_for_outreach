ca9a7370ae73ee3ebcf19cc91af223d6b1a89f56
cmaspi <chiragmehta1010@gmail.com>
2022-02-22 19:41:27 +0530

    Added archive functionality, and improved readability
    changed i,j variables to appropriate variables, changed a for loop to
    list comprehension. added few comments in the code, modified README to
    account for the changes

--- crawler.py
+++ crawler.py
@@ -3,30 +3,48 @@ import pandas as pd
 
 out_ = []
 year = input("Please enter year of search: ")
+print("It may take a few minutes")
 
+# Reading the list of faculties
 with open('faculty.txt') as f:
-    faculty = f.readlines()
+    faculties = f.readlines()
 
-for i in range(len(faculty)):
-    faculty[i] = faculty[i].rstrip()
+# reading the archive of papers
+df = pd.read_csv('archive.csv')
+# extracting the title column
+archive = df['Title'].to_numpy()
 
-for i in faculty:
-    search_query = scholarly.search_author(i)
+# removing the newline character from end
+archive = [ title.rstrip() for title in archive ]
+
+# removing the newline character from end
+faculties = [ faculty.rstrip() for faculty in faculties ]
+
+for faculty in faculties:
+    search_query = scholarly.search_author(faculty)
     author = scholarly.fill(next(search_query), sections=['publications'])
-    for j in author['publications']:
-        if 'pub_year' in j['bib']:
-            if j['bib']['pub_year'] == year:
-                j = scholarly.fill(j)
-                out_.append({'Author':j['bib']['author'], 'Title':j['bib']['title'], 'Publication_year':j['bib']['pub_year']})
-                if 'pub_url' in j:
-                    out_[-1]['url'] = j['pub_url']
-                if 'conference' in j['bib']:
-                    out_[-1]['conference'] = j['bib']['conference']
-                if 'journal' in j['bib']:
-                    out_[-1]['journal'] = j['bib']['journal']
-                if 'publisher' in j['bib']:
-                    out_[-1]['publisher'] = j['bib']['publisher']
+    for publication in author['publications']:
+        if 'pub_year' in publication['bib']:
+            if publication['bib']['pub_year'] == year and publication['bib']['title'] not in archive:
+                publication = scholarly.fill(publication)
+                out_.append({'Author':publication['bib']['author'],\
+                     'Title':publication['bib']['title'], 'Publication_year':publication['bib']['pub_year']})
+                if 'pub_url' in publication:
+                    out_[-1]['url'] = publication['pub_url']
+                if 'conference' in publication['bib']:
+                    out_[-1]['conference'] = publication['bib']['conference']
+                if 'journal' in publication['bib']:
+                    out_[-1]['journal'] = publication['bib']['journal']
+                if 'publisher' in publication['bib']:
+                    out_[-1]['publisher'] = publication['bib']['publisher']
+                # adds a counter to number of papers being added
+                print('+',end='', flush=True)
+print()
 
 df = pd.DataFrame(out_)   
 
 df.to_csv('pubs.csv')
+
+# appending the new papers in archive
+df.to_csv('archive.csv',mode='a',header=False, index=False)
+
